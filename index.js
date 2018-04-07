02786b5390e4ef94baa249f4bf96a5075becddf1
Jacob <jacob.clarke2@gmail.com>
2018-04-07 21:27:38 +0100

    Fixed port

--- index.js
+++ index.js
@@ -24,4 +24,4 @@ app.get('/*:*', async (req, res) => {
     res.json(data);
 })
 
-app.listen(process.env.port || 3000, () => console.log('listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}`))
